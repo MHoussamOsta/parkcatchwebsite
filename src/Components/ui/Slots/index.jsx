f3243ecfbd222460db55cbffda61070157e9904c
MHoussamOsta <138572115+MHoussamOsta@users.noreply.github.com>
2024-06-03 02:33:31 +0300

    Update index.jsx

--- src/Components/ui/Slots/index.jsx
+++ src/Components/ui/Slots/index.jsx
@@ -24,7 +24,7 @@ const Slots = () => {
         token: userData.token,
       };
       const response = await axios.post(
-        "http://127.0.0.1:8000/api/spots",
+        "http://16.170.35.122/api/spots",
         dataForm
       );
 
