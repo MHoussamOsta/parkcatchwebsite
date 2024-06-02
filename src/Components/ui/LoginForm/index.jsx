9bb74bac0001dbb4dbd73fc947113388ea4d391e
MHoussamOsta <138572115+MHoussamOsta@users.noreply.github.com>
2024-06-03 02:37:14 +0300

    Update index.jsx

--- src/Components/ui/LoginForm/index.jsx
+++ src/Components/ui/LoginForm/index.jsx
@@ -35,7 +35,7 @@ const LoginForm = () => {
           email: inputValues['Email'],
           password: inputValues['Password'],
         }
-        const response = await axios.post("http://localhost:8000/api/login", dataForm);
+        const response = await axios.post("http://16.170.35.122/api/login", dataForm);
     
         const userData = response.data.data;
         const userToken = response.data.data.token;
